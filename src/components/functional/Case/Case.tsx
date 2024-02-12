47030483c7c5b4dbd497b1b4650742f9d2d6df80
AmeliaSimone <108696216+AmeliaSimone@users.noreply.github.com>
2024-02-12 19:30:00 -0300

    actualizacion lunes 12 feb

--- src/components/functional/Case/Case.tsx
+++ src/components/functional/Case/Case.tsx
@@ -18,6 +18,7 @@ import TextBox from "@/components/ui/TextBox";
 import ComboBox from "@/components/ui/ComboBox";
 import Icon from "@/components/ui/Icon";
 import Capsule from "@/components/ui/Capsule";
+import Button from "@/components/ui/Button";
 
 import styles from "./Case.module.scss";
 
@@ -26,136 +27,133 @@ export const Case = () => {
     <Screen>
       <Header />
       <Body className={styles.body}>
-        <Panel className={styles.panel}>
-          <Section width="550px" className={styles.section}>
-            <TextBox className={styles.textBox}
+        <Panel className={styles.panelValues}>
+          <Section width="100%" className={styles.section}>
+            <TextBox
+              className={styles.textBox}
               label="Empresa"
               text="BCI Seguros Generales S.A."
               width="100%"
             />
-            <TextBox className={styles.textBox}
+            <TextBox
+              className={styles.textBox}
               label="Beneficiario"
               text="MARIA BERNARDITA ISABEL MUÑOZ PARADA"
               width="100%"
             />
           </Section>
-          <Section width="550px" wrap="wrap" className={styles.section}>
-            <TextBox className={styles.textBox}
+          <Section width="100%" wrap="wrap" className={styles.section}>
+            <TextBox
+              className={styles.textBox}
               label="Producto"
               text="Sala De Urgencua Familiar 21UF"
               width="100%"
             />
-            <ComboBox className={styles.comboBox}
+            <ComboBox
+              className={styles.comboBox}
               label="Asistencia"
               data={["Urgecia medica por enfermedad", "Otro Item"]}
               width="100%"
             />
-            <TextBox className={styles.textBox}
+            <TextBox
+              className={styles.textBox}
               label="Monto disponible"
               text="0"
               width="190px"
             />
-            <TextBox className={styles.textBox} label="Eventos" text="0" width="120px" />
-            <TextBox className={styles.textBox} label="Límite" text="" width="228px" />
-            <TextBox className={styles.textBox}
+            <TextBox
+              className={styles.textBox}
+              label="Eventos"
+              text="0"
+              width="120px"
+            />
+            <TextBox
+              className={styles.limite}
+              label="Límite"
+              text=""
+              width="210px"
+            />
+            <TextBox
+              className={styles.textBox}
               label="Monto Utilizado"
               text="0"
               width="190px"
             />
-            <TextBox className={styles.textBox} label="Eventos usados" text="0" width="120px" />
+            <TextBox
+              className={styles.textBox}
+              label="Eventos usados"
+              text="0"
+              width="120px"
+            />
           </Section>
           <Section className={styles.section}>
-            <Table height="285px" className={styles.table}>
+            <Table height="285px" className={styles.tableValues}>
               <TableHeader>
-                <TableCell width="265px">Dato</TableCell>
-                <TableCell width="275px">Valor</TableCell>
+                <TableCell>Dato</TableCell>
+                <TableCell>Valor</TableCell>
                 <TableCellEnd></TableCellEnd>
               </TableHeader>
               <TableDetail>
                 <TableRow>
-                  <TableCell width="265px">Tipo de previsión</TableCell>
-                  <TableCell width="275px">ISAPRE</TableCell>
+                  <TableCell>Tipo de previsión</TableCell>
+                  <TableCell>ISAPRE</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell width="265px">Fecha de nacimiento</TableCell>
-                  <TableCell width="275px">20/10/1986</TableCell>
+                  <TableCell>Fecha de nacimiento</TableCell>
+                  <TableCell>20/10/1986</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell width="265px">Rut del beneficiario</TableCell>
-                  <TableCell width="275px">16.360.110-9</TableCell>
+                  <TableCell>Rut del beneficiario</TableCell>
+                  <TableCell>16.360.110-9</TableCell>
                 </TableRow>
               </TableDetail>
             </Table>
           </Section>
         </Panel>
 
-        <Panel>
-          <Section aling="space-between"  className={styles.section}>
-            <Table height="400px" className={styles.table}>
+        <Panel className={styles.panelEvents}>
+          <Section className={styles.section} aling="space-between">
+            <Table height="400px" className={styles.tableEvents}>
               <TableHeader>
-                <TableCell width="95px">Fecha</TableCell>
-                <TableCell width="60px">Hora</TableCell>
-                <TableCell width="180px">Operador</TableCell>
-                <TableCell width="204px">Etapa/Acción</TableCell>
-                <TableCell width="40px"></TableCell>
+                <TableCell>Fecha</TableCell>
+                <TableCell>Hora</TableCell>
+                <TableCell>Operador</TableCell>
+                <TableCell>Etapa/Acción</TableCell>
+                <TableCell></TableCell>
                 <TableCellEnd></TableCellEnd>
               </TableHeader>
               <TableDetail>
                 <TableRow>
-                  <TableCell width="95px" aling="center">
-                    07/01/2024
-                  </TableCell>
-                  <TableCell width="60px" aling="center">
-                    19:52
-                  </TableCell>
-                  <TableCell width="180px" aling="start">
-                    Lia García
-                  </TableCell>
-                  <TableCell width="204px" aling="start">
-                    Datos del servicio
-                  </TableCell>
-                  <TableCell width="40px" aling="center">
+                  <TableCell aling="center">07/01/2024</TableCell>
+                  <TableCell aling="center">19:52</TableCell>
+                  <TableCell aling="start">Lia García</TableCell>
+                  <TableCell aling="start">Datos del servicio</TableCell>
+                  <TableCell aling="center">
                     <Icon name="search" color="#979797"></Icon>
                   </TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell width="95px" aling="center">
-                    07/01/2024
-                  </TableCell>
-                  <TableCell width="60px" aling="center">
-                    19:52
-                  </TableCell>
-                  <TableCell width="180px" aling="start">
-                    Lia García
-                  </TableCell>
-                  <TableCell width="204px" aling="start">
-                    Datos del evento
-                  </TableCell>
-                  <TableCell width="40px" aling="center">
+                  <TableCell aling="center">07/01/2024</TableCell>
+                  <TableCell aling="center">19:52</TableCell>
+                  <TableCell aling="start">Lia García</TableCell>
+                  <TableCell aling="start">Datos del evento</TableCell>
+                  <TableCell aling="center">
                     <Icon name="search" color="#979797"></Icon>
                   </TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell width="95px" aling="center">
-                    07/01/2024
-                  </TableCell>
-                  <TableCell width="60px" aling="center">
-                    19:52
-                  </TableCell>
-                  <TableCell width="180px" aling="start">
-                    Lia García
-                  </TableCell>
-                  <TableCell width="204px" aling="start">
-                    Datos del beneficiario
-                  </TableCell>
-                  <TableCell width="40px" aling="center">
+                  <TableCell aling="center">07/01/2024</TableCell>
+                  <TableCell aling="center">19:52</TableCell>
+                  <TableCell aling="start">Lia García</TableCell>
+                  <TableCell aling="start">Datos del beneficiario</TableCell>
+                  <TableCell aling="center">
                     <Icon name="search" color="#979797"></Icon>
                   </TableCell>
                 </TableRow>
               </TableDetail>
             </Table>
-            
-            
+            <Capsule text="3 acciones" backgroundColor="blue" />
+            <Button text="Chat" iconName="chat" backgroundColor="#03495C" />
           </Section>
         </Panel>
       </Body>
